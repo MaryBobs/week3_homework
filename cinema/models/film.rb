ca746fb111581e06beaa9e481ae6a6c687fcafce
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-03 10:13:38 +0000

    C and D functions added

---
+++ cinema/models/film.rb
@@ -0,0 +1,36 @@
+require_relative("../db/sql_runner.rb")
+
+class Film
+
+attr_reader :id, :title, :price
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @title = options['title']
+  @price = options['price']
+end
+
+def save()
+  sql = "INSERT INTO films
+  (title, price)
+  VALUES
+  ($1, $2)
+  RETURNING id;"
+  values = [@title, @price]
+  @id = SqlRunner.run(sql, values)[0]['id'].to_i
+end
+
+def delete()
+  sql = "DELETE FROM films
+  WHERE id = $1;"
+  values = [@id]
+  SqlRunner.run(sql, values)
+end
+
+def self.delete_all()
+  sql = "DELETE FROM films"
+  SqlRunner.run(sql)
+end
+
+
+end
